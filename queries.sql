bc18ac0e6552be3f830e21c536669b24c8e6281c
jpharvey8 <92167429+jpharvey8@users.noreply.github.com>
2022-01-30 19:57:36 -0500

    file upload

--- queries.sql
+++ queries.sql
@@ -84,11 +84,11 @@ LEFT JOIN dept_emp as de
 ON ce.emp_no = de.emp_no
 GROUP BY de.dept_no
 
--- Creating additonal lists: Employee info, Management and Dept. retirees
-
+-- Additional lists: Employee info, Management, and Dept. retirees
 SELECT * FROM salaries
 ORDER BY to_date DESC;
 
+-- List 1: Employee info 
 SELECT e.emp_no,
     e.first_name,
 e.last_name,
@@ -104,5 +104,67 @@ ON (e.emp_no = de.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
      AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
 	      AND (de.to_date = '9999-01-01');
+		  
+-- List 2: managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+-- INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+		
+-- List 3: Dept retirees	
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+-- INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+-- Questions to ask: 
+-- What's going on with the salaries?
+-- Why are there only five active managers for nine departments?
+-- Why are some employees appearing twice?
+
+-- Tailored lists 
+-- List for sales team 
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO dept_info_sales
+FROM current_emp as ce
+INNER JOIN dept_emp as de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments as d
+ON (de.dept_no = d.dept_no)
+WHERE (d.dept_name = 'Sales')
+
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO dept_info_sales_devlp
+FROM current_emp as ce
+INNER JOIN dept_emp as de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments as d
+ON (de.dept_no = d.dept_no)
+WHERE d.dept_name in ('Sales','Development')
+ORDER BY d.dept_name DESC;
+
+SELECT * FROM dept_info_sales_devlp
 
 
+SELECT * FROM dept_info_sales
